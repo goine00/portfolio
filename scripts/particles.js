f048e7805ef328032a835282c2e72655f5e6e522
goine <goine798@gmail.com>
2025-11-09 19:49:39 +0700

    add anim

--- scripts/particles.js
+++ scripts/particles.js
@@ -4,5 +4,17 @@ const particle_count = 20;
 for (let i = 0; i < particle_count; i++) {
     const div = document.createElement('div');
     div.classList.add('particle');
+
+    const left = Math.random() * 100;
+    const top = Math.random() * 100;
+    div.style.left = `${left}%`;
+    div.style.top = `${top}%`;
+
+    const duration = 12 + Math.random() * 8;
+    div.style.animationDuration = `${duration}s`;
+
+    const delay = Math.random() * 5;
+    div.style.animationDelay = `${delay}s`;
+
     container.appendChild(div);
 }
